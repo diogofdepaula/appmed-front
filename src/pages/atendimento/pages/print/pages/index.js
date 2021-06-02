68dd590b0f308a9a88128a5363a2b542c3307b21
Diogo F de Paula <diogofdepaula@gmail.com>
2021-06-02 11:19:52 -0300

    Leflunomida como receita separada

--- src/pages/atendimento/pages/print/pages/index.js
+++ src/pages/atendimento/pages/print/pages/index.js
@@ -31,11 +31,25 @@ const PrintJob = () => {
                         :
                         <FactoryReceitas listPresc={l.prescricoes} via={"Estado"} tipo={"lme"} />
                     }
+                    {/* Receita de Leflunomida */}
+                    {l.prescricoes.medicamento.farmaco === "Leflunomida" &&
+                        [...Array(6).keys()].map(d =>
+                            <div key={d}>
+                                {/* tem que passar o valor de cada mes da prescricao para cada receita de cada mês se não sai somente a soma */}
+                                <FactoryReceitas listPresc={l.prescricoes.filter(t => t.prescricoes.medicamento.farmaco === "Leflunomida")} via={"Estado"} mes={d} tipo={"lme"} />
+
+                                {/*  TENTAR FAZER DEPOIS DUAS RECEITAS POR FOLHA DE PAPEL */}
+                            </div>
+                        )
+                    }
                     {/* Medicamentos não controlados */}
                     {/* não passar a variável mês, para dar undifined lá nos componentes internos e saber, saber que é via paciente (aí não precisa passar o via paciente) */}
                     <FactoryReceitas listPresc={l.prescricoes} via={"paciente"} tipo={"lme"} />
                 </div>
             )
+
+
+
         )
 
         //print prescricoesSelecionadas
