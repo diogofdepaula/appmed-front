3b2f4b90599554ee762338aad0333a729f1ea354
Diogo F de Paula <diogofdepaula@gmail.com>
2021-06-14 15:15:11 -0300

    undefined na linha 2

--- src/pages/atendimento/pages/print/pages/receita/component/linha2.js
+++ src/pages/atendimento/pages/print/pages/receita/component/linha2.js
@@ -44,7 +44,7 @@ const Linha2 = ({ tipo }) => {
 
         console.log(prescricao);
 
-        if (impressao.local === 'consultorio' && tipo !== 'lme') {
+        if (impressao.local === 'consultorio' && tipo !== 'lme' && tipo !== undefined) {
             prescricao.medicamento.nomescomerciais?.map((n, i) => {
                 if (n.id === prescricao.medicamento.nomescomerciais[0].id) {
                     return texto = texto.concat(n.nomefantasia)
