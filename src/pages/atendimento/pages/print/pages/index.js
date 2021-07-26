7391814ffb35da50f32201976bc0141e9564789e
Diogo F de Paula <diogofdepaula@gmail.com>
2021-07-26 17:35:50 -0300

    termo consentimento para o fim

--- src/pages/atendimento/pages/print/pages/index.js
+++ src/pages/atendimento/pages/print/pages/index.js
@@ -46,11 +46,12 @@ const PrintJob = () => {
                     {/* não passar a variável mês, para dar undifined lá nos componentes internos e saber, saber que é via paciente (aí não precisa passar o via paciente) */}
                     <FactoryReceitas listPresc={l.prescricoes} via={"paciente"} tipo={"lme"} />
 
-                    {impressao.termosSelecionados.length > 0 && <TermoConsentimento />}
                 </div>
             )
         )
 
+        impressao.termosSelecionados.length > 0 && jobs.push(<TermoConsentimento />)
+        
         //print prescricoesSelecionadas
         if (impressao.prescricoesSelecionadas.length > 0) {
 
