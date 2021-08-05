0eb6944b2e9c53678af1d7cddbe038c87b8fde98
Diogo F de Paula <diogofdepaula@gmail.com>
2021-08-05 14:28:43 -0300

    Campo transf nome caixa alta em só a primeira alta

--- src/pages/cadastro/clientes/components/clienteform.js
+++ src/pages/cadastro/clientes/components/clienteform.js
@@ -1,4 +1,4 @@
-import { Box, FormControlLabel, Grid, Radio, RadioGroup, TextField } from '@material-ui/core';
+import { Box, Divider, FormControlLabel, Grid, Radio, RadioGroup, TextField } from '@material-ui/core';
 import { format } from 'date-fns';
 import React, { useContext } from 'react';
 import ReactInputMask from 'react-input-mask';
@@ -23,190 +23,217 @@ const ClienteForm = () => {
         }
     }
 
+    const handleNome = event => {
+
+        const str = event.target.value
+            .toLowerCase()
+            .split(" ")
+            .map(m => m.length > 2 ? m.charAt(0).toUpperCase() + m.slice(1) : m)
+            .join(" ")
+        setClienteEdit({ ...clienteEdit, [event.target.name]: str })
+    }
+
 
     return (
         <div>
-            <Grid container spacing={2} >
-                <Grid container item spacing={2} >
-                    <Grid item xs={9} >
-                        <TextField
-                            autoFocus
-                            fullWidth
-                            name="nome"
-                            label="Nome completo"
-                            variant="outlined"
-                            value={clienteEdit.nome}
-                            onChange={handleChange}
-                        />
+            <Box mb={2}>
+                <Grid container spacing={2} >
+                    <Grid container item spacing={2} >
+                        <Grid item xs={9} >
+                            <TextField
+                                autoFocus
+                                fullWidth
+                                name="nome"
+                                label="Nome completo"
+                                variant="outlined"
+                                value={clienteEdit.nome}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs={3}>
+                            <RadioGroup aria-label="gender" name="gender" onChange={handleChange}>
+                                <Box display="flex" >
+                                    <FormControlLabel
+                                        name='sexo'
+                                        value="feminino"
+                                        control={<Radio />}
+                                        checked={clienteEdit.sexo === "feminino"}
+                                        label="Feminino"
+                                        variant="outlined"
+                                    />
+                                    <FormControlLabel
+                                        name='sexo'
+                                        value="masculino"
+                                        control={<Radio />}
+                                        checked={clienteEdit.sexo === "masculino"}
+                                        label="Masculino"
+                                        variant="outlined"
+                                    />
+                                </Box>
+                            </RadioGroup>
+                        </Grid>
                     </Grid>
-                    <Grid item xs={3}>
-                        <RadioGroup aria-label="gender" name="gender" onChange={handleChange}>
-                            <Box display="flex" >
-                                <FormControlLabel
-                                    name='sexo'
-                                    value="feminino"
-                                    control={<Radio />}
-                                    checked={clienteEdit.sexo === "feminino"}
-                                    label="Feminino"
+                    <Grid container item spacing={2}>
+                        <Grid item xs>
+                            <TextField
+                                name="nascimento"
+                                label="Data de Nascimento"
+                                type="date"
+                                variant="outlined"
+                                InputLabelProps={{
+                                    shrink: true,
+                                }}
+                                value={clienteEdit.nascimento}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+
+                        <Grid item xs>
+                            <ReactInputMask
+                                mask="999"
+                                value={clienteEdit.peso}
+                                maskChar=" "
+                                onChange={handleChange}
+                            >
+                                {() => <TextField
+                                    name="peso"
                                     variant="outlined"
-                                />
-                                <FormControlLabel
-                                    name='sexo'
-                                    value="masculino"
-                                    control={<Radio />}
-                                    checked={clienteEdit.sexo === "masculino"}
-                                    label="Masculino"
+                                    label="Peso(Kg)"
+                                />}
+                            </ReactInputMask>
+                        </Grid>
+                        <Grid item xs>
+                            <ReactInputMask
+                                mask="999"
+                                value={clienteEdit.altura}
+                                maskChar=" "
+                                onChange={handleChange}
+                            >
+                                {() => <TextField
+                                    name="altura"
                                     variant="outlined"
-                                />
-                            </Box>
-                        </RadioGroup>
+                                    label="Altura(cm)"
+                                />}
+                            </ReactInputMask>
+                        </Grid>
+                        <Grid item xs>
+                            <ReactInputMask
+                                mask="(99)99999-9999"
+                                value={clienteEdit.telefone}
+                                maskChar="_"
+                                onChange={handleChange}
+                            >
+                                {() => <TextField
+                                    name="telefone"
+                                    variant="outlined"
+                                    label="Telefone"
+                                />}
+                            </ReactInputMask>
+                        </Grid>
+                        <Grid item xs>
+                            <ReactInputMask
+                                mask="(99)99999-9999"
+                                value={clienteEdit.celular}
+                                maskChar="_"
+                                onChange={handleChange}
+                            >
+                                {() => <TextField
+                                    name="celular"
+                                    variant="outlined"
+                                    label="Celular"
+                                />}
+                            </ReactInputMask>
+                        </Grid>
+                    </Grid>
+                    <Grid container item spacing={2}>
+                        <Grid item xs>
+                            <ReactInputMask
+                                mask="999.9999.9999.9999"
+                                value={clienteEdit.cns}
+                                maskChar="_"
+                                onChange={handleChange}
+                            >
+                                {() => <TextField
+                                    fullWidth
+                                    name="cns"
+                                    variant="outlined"
+                                    label="CNS"
+                                />}
+                            </ReactInputMask>
+                        </Grid>
+                        <Grid item xs>
+                            <ReactInputMask
+                                mask="999.999.999-99"
+                                value={clienteEdit.cpf}
+                                maskChar="_"
+                                onChange={handleChange}
+                            >
+                                {() => <TextField
+                                    fullWidth
+                                    name="cpf"
+                                    variant="outlined"
+                                    label="CPF"
+                                />}
+                            </ReactInputMask>
+                        </Grid>
+                        <Grid item xs>
+                            <TextField
+                                fullWidth
+                                type="email"
+                                name="email"
+                                variant="outlined"
+                                label="Email"
+                                value={clienteEdit.email}
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Grid container item spacing={2}>
+                        <Grid item xs sm>
+                            <TextField
+                                fullWidth
+                                name="mae"
+                                variant="outlined"
+                                label="Nome da Mãe"
+                                value={clienteEdit.mae}
+                                In
+                                onChange={handleChange}
+                            />
+                        </Grid>
+                        <Grid item xs>
+                            <TextField
+                                fullWidth
+                                variant="outlined"
+                                name="endereco"
+                                label="Endereço"
+                                value={clienteEdit.endereco}
+                                onChange={handleChange}
+                            />
+                        </Grid>
                     </Grid>
                 </Grid>
-                <Grid container item spacing={2}>
-                    <Grid item xs>
-                        <TextField
-                            name="nascimento"
-                            label="Data de Nascimento"
-                            type="date"
-                            variant="outlined"
-                            InputLabelProps={{
-                                shrink: true,
-                            }}
-                            value={clienteEdit.nascimento}
-                            onChange={handleChange}
-                        />
-                    </Grid>
-                    <Grid item xs>
+            </Box>
+            <Divider />
+            <Box mt={2}>
+                <Grid container spacing={2}>
+                    <Grid item>
                         <TextField
                             variant="outlined"
                             label="Cole Nascimento"
                             onChange={(e) => handleChangeNascimento(e)}
                         />
                     </Grid>
-                    <Grid item xs>
-                        <ReactInputMask
-                            mask="999"
-                            value={clienteEdit.peso}
-                            maskChar=" "
-                            onChange={handleChange}
-                        >
-                            {() => <TextField
-                                name="peso"
-                                variant="outlined"
-                                label="Peso(Kg)"
-                            />}
-                        </ReactInputMask>
-                    </Grid>
-                    <Grid item xs>
-                        <ReactInputMask
-                            mask="999"
-                            value={clienteEdit.altura}
-                            maskChar=" "
-                            onChange={handleChange}
-                        >
-                            {() => <TextField
-                                name="altura"
-                                variant="outlined"
-                                label="Altura(cm)"
-                            />}
-                        </ReactInputMask>
-                    </Grid>
-                    <Grid item xs>
-                        <ReactInputMask
-                            mask="(99)99999-9999"
-                            value={clienteEdit.telefone}
-                            maskChar="_"
-                            onChange={handleChange}
-                        >
-                            {() => <TextField
-                                name="telefone"
-                                variant="outlined"
-                                label="Telefone"
-                            />}
-                        </ReactInputMask>
-                    </Grid>
-                    <Grid item xs>
-                        <ReactInputMask
-                            mask="(99)99999-9999"
-                            value={clienteEdit.celular}
-                            maskChar="_"
-                            onChange={handleChange}
-                        >
-                            {() => <TextField
-                                name="celular"
-                                variant="outlined"
-                                label="Celular"
-                            />}
-                        </ReactInputMask>
-                    </Grid>
-                </Grid>
-                <Grid container item spacing={2}>
-                    <Grid item xs>
-                        <ReactInputMask
-                            mask="999.9999.9999.9999"
-                            value={clienteEdit.cns}
-                            maskChar="_"
-                            onChange={handleChange}
-                        >
-                            {() => <TextField
-                                fullWidth
-                                name="cns"
-                                variant="outlined"
-                                label="CNS"
-                            />}
-                        </ReactInputMask>
-                    </Grid>
-                    <Grid item xs>
-                        <ReactInputMask
-                            mask="999.999.999-99"
-                            value={clienteEdit.cpf}
-                            maskChar="_"
-                            onChange={handleChange}
-                        >
-                            {() => <TextField
-                                fullWidth
-                                name="cpf"
-                                variant="outlined"
-                                label="CPF"
-                            />}
-                        </ReactInputMask>
-                    </Grid>
-                    <Grid item xs>
+                    <Grid item xs={6}>
                         <TextField
                             fullWidth
-                            type="email"
-                            name="email"
+                            name="nome"
+                            label="Cole nome em caixa alta"
                             variant="outlined"
-                            label="Email"
-                            value={clienteEdit.email}
-                            onChange={handleChange}
+                            onChange={(e) => handleNome(e)}
                         />
                     </Grid>
                 </Grid>
-                <Grid container item spacing={2}>
-                    <Grid item xs sm>
-                        <TextField
-                            fullWidth
-                            name="mae"
-                            variant="outlined"
-                            label="Nome da Mãe"
-                            value={clienteEdit.mae}
-                            In
-                            onChange={handleChange}
-                        />
-                    </Grid>
-                    <Grid item xs>
-                        <TextField
-                            fullWidth
-                            variant="outlined"
-                            name="endereco"
-                            label="Endereço"
-                            value={clienteEdit.endereco}
-                            onChange={handleChange}
-                        />
-                    </Grid>
-                </Grid>
-            </Grid>
+            </Box>
         </div>
     )
 }
