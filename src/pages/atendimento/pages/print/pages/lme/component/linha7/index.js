90cefc2a4682e17e78875b6c3f27f0d45174aea8
Diogo F de Paula <diogofdepaula@gmail.com>
2021-08-24 17:08:00 -0300

    Aumento progressivo da fonte da LME

--- src/pages/atendimento/pages/print/pages/lme/component/linha7/index.js
+++ src/pages/atendimento/pages/print/pages/lme/component/linha7/index.js
@@ -6,35 +6,39 @@ const Linha7LME = () => {
 
     const lme = useContext(LMEPrintContext)
 
+    
+    const inputRef = useRef(null);
+    const [fontsize, setFontSize] = useState(10)
+    
+    const ChangeSize = useCallback((param) => {
+        //366  a progressão do tamanho conforme aumenta a fonte  208-244-280-316-352
+        if (param < 316 && fontsize < 22)  {      
+            setFontSize(fontsize + 4)
+        }
+    }, [fontsize])
+    
+    useEffect(() => {
+        ChangeSize(inputRef.current.offsetHeight)
+    }, [ChangeSize]);
+
     const Texto = () => {
 
         let texto = <div />
 
         if (lme.anamnese) {
             texto =
-                <Typography component={'span'} variant={fontsize} align={'justify'}>
+                <Typography component={'span'} variant={'h6'} align={'justify'}>
+                    <Box fontSize={fontsize} >
                     {lme.anamnese.split("\n").map((i, key) => {
                         return <div key={Math.random() * 1000}>{i}</div>;
                     })}
+                    </Box>
                 </Typography>
         } else {
             texto = <Box style={{ color: "white" }}>-</Box>
         }
         return texto
     }
-
-    const inputRef = useRef(null);
-    const [fontsize, setFontSize] = useState('h6')
-
-    const ChangeSize = useCallback((param) => {
-        if (param > 366) {
-            setFontSize('body2')
-        }
-    }, [])
-
-    useEffect(() => {
-        ChangeSize(inputRef.current.offsetHeight)
-    }, [ChangeSize]);
 
     return (
         <>
@@ -45,7 +49,7 @@ const Linha7LME = () => {
                             <Grid item>
                                 <Box mt={-1} ml={2} display="flex">
                                     <Typography component={'span'} variant="caption" noWrap={true} >
-                                        <Box bgcolor="white" px={1}>11 e 12 - História clínica (Anamnese e tratamento prévio)</Box>
+                                        <Box bgcolor="white" px={1}>11 e 12 - Relatório médico (Anamnese e tratamento prévio)</Box>
                                     </Typography>
                                 </Box>
                             </Grid>
