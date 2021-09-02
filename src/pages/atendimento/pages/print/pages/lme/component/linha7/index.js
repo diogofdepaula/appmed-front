7d3c6ff3dd6875a0a9f46b9570a715dbf0625beb
Diogo F de Paula <diogofdepaula@gmail.com>
2021-09-02 20:36:36 -0300

    limpando comentários

--- src/pages/atendimento/pages/print/pages/lme/component/linha7/index.js
+++ src/pages/atendimento/pages/print/pages/lme/component/linha7/index.js
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
 import React, { useCallback, useContext, useEffect, useRef, useState } from 'react'
 import { LMEPrintContext } from '../..'
 
@@ -6,50 +6,44 @@ const Linha7LME = () => {
 
     const lme = useContext(LMEPrintContext)
 
-    const inputRef = useRef(null);
+    const insideRef = useRef(null);
+    const outsideRef = useRef(null);
     const [fontsize, setFontSize] = useState(10)
-    const [conter, setConter] = useState(0)
 
     const ChangeSize = useCallback((param) => {
-        //366  a progressão do tamanho conforme aumenta a fonte  208-244-280-316-352
-        console.log('dentro do callback', inputRef.current?.offsetHeight, '  ', fontsize);
-        if (param < 316 && fontsize < 22) {
-            setFontSize(fontsize + 4)
+        // a cada 2 de fontsize cresce 18-36px de insideRef.current?.offsetHeight
+        // 24 x 2 + 2(de margem de erro)
+        if ((insideRef.current?.offsetHeight + 50) < outsideRef.current?.offsetHeight && fontsize < 24) {
+            setFontSize(fontsize + 2)
         }
-
     }, [fontsize])
 
     useEffect(() => {
-        // ChangeSize(inputRef.current.offsetHeight)
-        if (conter !== 4) {
-            setConter(prev => prev + 1)
-        }
-    }, [ChangeSize, conter]);
-
-    console.log('fora do callback', inputRef.current?.offsetHeight, '  ', fontsize, '  ', conter);
+        ChangeSize(insideRef.current.offsetHeight)
+    }, [ChangeSize]);
 
     return (
         <>
-            {/* <Grid container item xs> */}
-                <Box mt={2} flexGrow={1} width={1} border={1} borderColor="black" component="span" display="block" >
-                    {/* <Grid container item xs direction="column" justify="flex-end" alignItems="stretch">
-                        <Grid item> */}
-
-
-                            <Box mt={-1} ml={2} display="flex">
-                                <Typography component={'span'} variant="caption" noWrap={true} >
-                                    <Box bgcolor="white" px={1}>11 e 12 - Relatório médico (Anamnese e tratamento prévio)</Box>
-                                </Typography>
-                            </Box>
-                        {/* </Grid>
-                        <Grid item xs> */}
-                            <Box p={1} height={1} style={{ whiteSpace: 'pre-wrap' }} ref={inputRef} >
-                                {lme.anamnese}
-                            </Box>
-                        {/* </Grid>
-                    </Grid> */}
+            <Box mt={2} flexGrow={1} width={1} border={1} borderColor="black" component="span" display="block" >
+                <Box mt={-1} ml={2} display="flex">
+                    <Typography component={'span'} variant="caption" noWrap={true} >
+                        <Box bgcolor="white" px={1}>11 e 12 - Relatório médico (Anamnese e tratamento prévio)</Box>
+                    </Typography>
                 </Box>
-            {/* </Grid> */}
+                <Box
+                    p={1}
+                    height={1}
+                    style={{ whiteSpace: 'pre-wrap' }}
+                    ref={outsideRef}
+                >
+                    <Box
+                        fontSize={fontsize}
+                        ref={insideRef}
+                    >
+                        {lme.anamnese}
+                    </Box>
+                </Box>
+            </Box>
         </>
     )
 }
