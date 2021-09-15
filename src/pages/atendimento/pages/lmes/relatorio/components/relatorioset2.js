d2c362c76662a2ca1c8d3c3e75d676b541385653
Diogo F de Paula <diogofdepaula@gmail.com>
2021-09-15 11:28:13 -0300

    Criterios diagnostico como classe separada

--- src/pages/atendimento/pages/lmes/relatorio/components/relatorioset2.js
+++ src/pages/atendimento/pages/lmes/relatorio/components/relatorioset2.js
@@ -1,6 +1,7 @@
 import { Box, Checkbox, FormControlLabel } from '@material-ui/core'
-import React, { useCallback, useContext, useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import { AtendimentoContext } from '../../../..'
+import Criterios from '../../../../component/criterios'
 
 const RelatorioSet2 = () => {
 
@@ -10,68 +11,12 @@ const RelatorioSet2 = () => {
         setLmeEdit({ ...lmeEdit, relatorio: { ...lmeEdit.relatorio, [event.target.name]: event.target.checked } })
     }
 
-    const [list, setList] = useState([])
-
-    const getList = useCallback(() => {
-        const ar = [
-            ['ara', 'Rigidez articular', lmeEdit.relatorio.ara],
-            ['arb', 'Artrite em três ou mais áreas', lmeEdit.relatorio.arb],
-            ['arc', 'Artrite em articulações das mãos', lmeEdit.relatorio.arc],
-            ['ard', 'Artrite simétrica', lmeEdit.relatorio.ard],
-            ['are', 'Nódulos reumatóides', lmeEdit.relatorio.are],
-            ['arf', 'Fator reumatóide sérico', lmeEdit.relatorio.arf],
-            ['arg', 'Alterações radiológicas', lmeEdit.relatorio.arg],
-        ]
-
-        const eap = [
-            ['eapa', 'Psoríase', lmeEdit.relatorio.eapa],
-            ['eapb', 'Artrite', lmeEdit.relatorio.eapb],
-            ['eapc', 'Entesite', lmeEdit.relatorio.eapc],
-            ['eapd', 'Dactilite', lmeEdit.relatorio.eapd],
-            ['eape', 'Alterações extrarticulares', lmeEdit.relatorio.eape],
-            ['eapf', 'Distrofia ungueal', lmeEdit.relatorio.eapf],
-            ['eapg', 'Proliferação óssea na radiografia', lmeEdit.relatorio.eapg],
-        ]
-
-        const eaa = [
-            ['eaaa', 'Dor lombar inflamatória', lmeEdit.relatorio.eaaa],
-            ['eaab', 'HLA-B27 detectado', lmeEdit.relatorio.eaab],
-            ['eaac', 'Sacroileíte / Lesões Axiais', lmeEdit.relatorio.eaac],
-            ['eaad', 'RM típica', lmeEdit.relatorio.eaad],
-            ['eaae', 'Alterações Extrarticulares', lmeEdit.relatorio.eaae],
-        ]
-
-        const arcid = ['M050', 'M051', 'M052', 'M053', 'M058', 'M060', 'M068']
-        const aijcid = ['M080']
-        const eaicid = ['M460', 'M461', 'M468']
-        const eapcid = ['M070', 'M073']
-        const eaacid = ['M45']
-
-        if (arcid.includes(lmeEdit.cid10)) {
-            setList(ar)
-        } else if (aijcid.includes(lmeEdit.cid10)) {
-            setList(ar)
-        } else if (eaicid.includes(lmeEdit.cid10)) {
-            setList(eap)
-        } else if (eapcid.includes(lmeEdit.cid10)) {
-            setList(eap)
-        } else if (eaacid.includes(lmeEdit.cid10)) {
-            setList(eaa)
-        }
-        // não sei como tirar toda essa lógica aqui de dentro.
-        // as const quando estão lá fora e são postas na lista de dependências dá loop infinito
-
-    }, [lmeEdit])
-
-    useEffect(() => {
-        getList()
-    }, [getList])
-
+    const list = Criterios(lmeEdit)
 
     return (
         <div>
             <Box m={2}>
-                {list?.map((w) =>
+                {list.map((w) =>
                     <Box key={w[0]}>
                         <FormControlLabel
                             control={
--- src/pages/atendimento/pages/print/pages/relatorio/component/linha7/index.js
+++ src/pages/atendimento/pages/print/pages/relatorio/component/linha7/index.js
@@ -3,61 +3,15 @@ import CheckBoxOutlineBlankIcon from '@material-ui/icons/CheckBoxOutlineBlank';
 import CheckBoxOutlinedIcon from '@material-ui/icons/CheckBoxOutlined';
 import React, { useContext } from 'react';
 import { LMEPrintContext } from '../..';
+import Criterios from '../../../../../../component/criterios';
 
 const Linha7Relatorio = () => {
 
     const lme = useContext(LMEPrintContext)
 
-    const Criterios = () => {
+    const OutrosCriterios = () => {
 
-        const ar = [
-            ['ara', 'Rigidez articular', lme.relatorio.ara],
-            ['arb', 'Artrite em três ou mais áreas', lme.relatorio.arb],
-            ['arc', 'Artrite em articulações das mãos', lme.relatorio.arc],
-            ['ard', 'Artrite simétrica', lme.relatorio.ard],
-            ['are', 'Nódulos reumatóides', lme.relatorio.are],
-            ['arf', 'Fator reumatóide sérico', lme.relatorio.arf],
-            ['arg', 'Alterações radiológicas', lme.relatorio.arg],
-        ]
-
-        const eap = [
-            ['eapa', 'Psoríase', lme.relatorio.eapa],
-            ['eapb', 'Artrite', lme.relatorio.eapb],
-            ['eapc', 'Entesite', lme.relatorio.eapc],
-            ['eapd', 'Dactilite', lme.relatorio.eapd],
-            ['eape', 'Alterações extrarticulares', lme.relatorio.eape],
-            ['eapf', 'Distrofia ungueal', lme.relatorio.eapf],
-            ['eapg', 'Proliferação óssea na radiografia', lme.relatorio.eapg],
-        ]
-
-        const eaa = [
-            ['eaaa', 'Dor lombar inflamatória', lme.relatorio.eaaa],
-            ['eaab', 'HLA-B27 detectado', lme.relatorio.eaab],
-            ['eaac', 'Sacroileíte / Lesões Axiais', lme.relatorio.eaac],
-            ['eaad', 'RM típica', lme.relatorio.eaad],
-            ['eaae', 'Alterações Extrarticulares', lme.relatorio.eaae],
-        ]
-
-        const arcid = ['M050', 'M051', 'M052', 'M053', 'M058', 'M060', 'M068']
-        const aijcid = ['M080']
-        const eaicid = ['M460', 'M461', 'M468']
-        const eapcid = ['M070', 'M073']
-        const eaacid = ['M45']
-
-        let list
-
-        if (arcid.includes(lme.cid10)) {
-            list = ar
-        } else if (aijcid.includes(lme.cid10)) {
-            list = ar
-        } else if (eaicid.includes(lme.cid10)) {
-            list = eap
-        } else if (eapcid.includes(lme.cid10)) {
-            list = eap
-        } else if (eaacid.includes(lme.cid10)) {
-            list = eaa
-        }
-
+        const list = Criterios(lme)
         return (
             <>
                 {list.map((w, i) =>
@@ -72,8 +26,8 @@ const Linha7Relatorio = () => {
                 )}
             </>
         )
-    }
 
+    }
 
     const ArtriteReumatoide2010 = () => {
 
@@ -130,7 +84,7 @@ const Linha7Relatorio = () => {
         if (crit === 4) {
             return <ArtriteReumatoide2010 />
         } else {
-            return <Criterios />
+            return <OutrosCriterios />
         }
     }
 
