677ef51a21ba8756489ff3b3dfd33e6ebeff1865
Diogo F de Paula <diogofdepaula@gmail.com>
2021-11-23 10:55:21 -0300

    apagando comentários

--- src/pages/atendimento/pages/lmes/components/lmevarset.js
+++ src/pages/atendimento/pages/lmes/components/lmevarset.js
@@ -11,10 +11,6 @@ const LMEVarSet = () => {
         setLmeEdit({ ...lmeEdit, [event.target.name]: event.target.value })
     }
 
-    // const handleChangeCheckBox = event => {
-    //     setLmeEdit({ ...lmeEdit, [event.target.name]: event.target.checked })
-    // }
-
     return (
         <div>
             <Box m={2}>
@@ -34,70 +30,6 @@ const LMEVarSet = () => {
                     />
                 </Box>
                 <AnamneseTips />
-                {/* <Box display="block">
-                    <Box>
-                        <FormGroup row>
-                            <FormControlLabel
-                                control={
-                                    <Checkbox
-                                        color='primary'
-                                        name="tratamentoprevio"
-                                        checked={lmeEdit.tratamentoprevio}
-                                        onChange={handleChangeCheckBox}
-                                    />}
-                                label='Tratamento Prévio'
-                            />
-                        </FormGroup>
-                        <TextField
-                            fullWidth
-                            variant='outlined'
-                            name="tratamentopreviotexto"
-                            label="Descrever tratamentos prévios"
-                            value={lmeEdit.tratamentopreviotexto}
-                            onChange={handleChange}
-                        />
-                    </Box>
-                    <Box>
-                        <FormControlLabel
-                            control={
-                                <Checkbox
-                                    color='primary'
-                                    name="atestadocapacidade"
-                                    value={lmeEdit.atestadocapacidade}
-                                    onChange={handleChangeCheckBox}
-                                />}
-                            label='Atestado de capacidade'
-                        />
-                    </Box>
-                    <Box>
-                        <TextField
-                            variant='outlined'
-                            name="preenchidopor"
-                            label="Preenchido por"
-                            value={lmeEdit.preenchidopor}
-                            onChange={handleChange}
-                        />
-                        <ReactInputMask
-                            mask="999.999.999-99"
-                            value={lmeEdit.preenchidoporCPF}
-                            maskChar="_"
-                            onChange={handleChange}
-                        >
-                            {() => <TextField
-                                variant='outlined'
-                                name="preenchidoporCPF"
-                                label="CPF do preenchedor"
-                            />}
-                        </ReactInputMask>
-                        <TextField
-                            variant='outlined'
-                            name="raca"
-                            label="Raça"
-                            value={lmeEdit.raca}
-                            onChange={handleChange}
-                        />
-                    </Box>
-                </Box> */}
             </Box>
         </div>
     )
