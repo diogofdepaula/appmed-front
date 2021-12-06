e69901527e817126fd14429b6ec5c5cab6aaae83
Diogo F de Paula <diogofdepaula@gmail.com>
2021-12-06 13:28:25 -0300

    favoritos do exames tips

--- src/pages/atendimento/pages/requisicao/unitarytips.js
+++ src/pages/atendimento/pages/requisicao/unitarytips.js
@@ -1,15 +1,7 @@
 import { Box, Chip } from "@material-ui/core"
+import Tuss from "./tuss"
 
 const UnitaryTips = ({ handleAddUnitaryTips, procedimentos, convenio }) => {
-
-    const exames = [
-        ["HMG", ["40304361", "202020380"],],
-        ["VHS", ["40304370", "202020150"],],
-        ["PCR", ["40308391", "202030083"],],
-        ["TGO", ["40302504", "202010643"],],
-        ["TGP", ["40302512", "202010651"],],
-        ["Cr", ["40301630", "202010317"],],
-    ]
 
     const sendParam = (param) => {
         let proc = procedimentos.filter(z => param.includes(z[0]))[0]
@@ -18,15 +10,17 @@ const UnitaryTips = ({ handleAddUnitaryTips, procedimentos, convenio }) => {
 
     return (
         <>
-            {exames.map((e, i) =>
-                <Box ml={1} key={i}>
-                    <Chip
-                        label={e[0]}
-                        variant="outlined"
-                        onClick={() => sendParam(e[1])}
-                    />
-                </Box>
-            )}
+            <Box display="flex">
+                {Tuss().filter(t => t[3] === true).map((e, i) =>
+                    <Box m={1} key={i}>
+                        <Chip
+                            label={e[4]}
+                            variant="outlined"
+                            onClick={() => sendParam(e[1])}
+                        />
+                    </Box>
+                )}
+            </Box>
         </>
     )
 }
