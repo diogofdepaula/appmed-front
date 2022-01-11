0a56cbd4918c2f1ddc4ac644e508f64a14c07de5
Diogo F de Paula <diogofdepaula@gmail.com>
2022-01-11 13:28:47 -0300

    remove UseStyle printDialog

--- src/pages/atendimento/pages/print/component/printdialog.js
+++ src/pages/atendimento/pages/print/component/printdialog.js
@@ -3,7 +3,6 @@ import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import IconButton from '@material-ui/core/IconButton';
 import Slide from '@material-ui/core/Slide';
-import { makeStyles } from '@material-ui/core/styles';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
@@ -12,23 +11,11 @@ import { useReactToPrint } from 'react-to-print';
 import { ImpressaoContext } from '../../..';
 import PrintJob from '../pages';
 
-const useStyles = makeStyles((theme) => ({
-    appBar: {
-        position: 'relative',
-    },
-    title: {
-        marginLeft: theme.spacing(2),
-        flex: 1,
-    },
-}));
-
 const Transition = React.forwardRef(function Transition(props, ref) {
     return <Slide direction="up" ref={ref} {...props} />;
 });
 
 const PrintDialog = ({ open, handleClose }) => {
-
-    const classes = useStyles();
 
     const { impressao } = useContext(ImpressaoContext)
 
@@ -56,12 +43,18 @@ const PrintDialog = ({ open, handleClose }) => {
     return (
         <div>
             <Dialog fullScreen open={open} onClose={handleClose} TransitionComponent={Transition}>
-                <AppBar className={classes.appBar}>
+                <AppBar position='relative'>
                     <Toolbar>
                         <IconButton edge="start" color="inherit" onClick={handleClose} aria-label="close">
                             <CloseIcon />
                         </IconButton>
-                        <Typography variant="h6" className={classes.title}>
+                        <Typography
+                            variant="h6"
+                            style={{
+                                marginLeft: 2,
+                                flex: 1,
+                            }}
+                        >
                             Lista de impressão
                         </Typography>
                         <Button autoFocus color="inherit" onClick={handlePrint}>
