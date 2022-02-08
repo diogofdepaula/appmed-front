b685e2e346e90d3fcdee3103442bf55f391cd985
Diogo F de Paula <diogofdepaula@gmail.com>
2022-02-08 20:35:51 -0300

    Google oAUth Sign-in

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,5 @@
 import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import { DialogContent } from '@mui/material';
 import Avatar from '@mui/material/Avatar';
 import { blue } from '@mui/material/colors';
 import Dialog from '@mui/material/Dialog';
@@ -7,7 +8,9 @@ import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
+import { Box } from '@mui/system';
 import React, { useContext } from 'react';
+import GoogleLogin from 'react-google-login';
 import { LoginContext } from '../App';
 
 const Login = ({ open, handleClose }) => {
@@ -22,19 +25,38 @@ const Login = ({ open, handleClose }) => {
     return (
         <>
             <Dialog open={open}>
-                <DialogTitle>Local de Atendimento</DialogTitle>
-                <List sx={{ pt: 0 }}>
-                    {locais.map(local => (
-                        <ListItem button onClick={() => handleListItem()} key={local}>
-                            <ListItemAvatar>
-                                <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
-                                    <AccountBalanceIcon />
-                                </Avatar>
-                            </ListItemAvatar>
-                            <ListItemText primary={local[0]} />
-                        </ListItem>
-                    ))}
-                </List>
+                <DialogContent dividers>
+                    <DialogTitle>Login do APPMED</DialogTitle>
+                    <Box
+                        sx={{ 
+                            display: 'flex',
+                            justifyContent: 'center',
+                        }}
+                    >
+                        <GoogleLogin
+                            clientId={process.env.REACT_GOOGLEID}
+                            buttonText="Google oAUth Sign-in"
+                            //onSuccess={responseGoogle}
+                            //onFailure={responseGoogle}
+                            cookiePolicy={'single_host_origin'}
+                        />
+                    </Box>
+                </DialogContent>
+                <DialogContent dividers>
+                    {/* <DialogTitle>Local de Atendimento</DialogTitle> */}
+                    <List sx={{ pt: 0 }}>
+                        {locais.map(local => (
+                            <ListItem button onClick={() => handleListItem()} key={local}>
+                                <ListItemAvatar>
+                                    <Avatar sx={{ bgcolor: blue[100], color: blue[600] }}>
+                                        <AccountBalanceIcon />
+                                    </Avatar>
+                                </ListItemAvatar>
+                                <ListItemText primary={local[0]} />
+                            </ListItem>
+                        ))}
+                    </List>
+                </DialogContent>
             </Dialog>
         </>
     )
