584f0ab1ac7d749110391d917a434ee8257e8ac5
Diogo F de Paula <diogofdepaula@gmail.com>
2022-04-01 09:17:41 -0300

    lmeappbar otimizado

--- src/components/appbar/lmemainappbar.js
+++ src/components/appbar/lmemainappbar.js
@@ -1,15 +1,34 @@
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import EditIcon from '@mui/icons-material/Edit';
-import HealingIcon from '@mui/icons-material/Healing';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import { IconButton, Tooltip } from '@mui/material';
 import React, { useContext } from 'react';
 import { AtendimentoContext, AtendimentoNavigateContext } from '../../pages/atendimento';
 
-const LmesMainAppBar = () => {
+const ReiniciarBtn = () => {
+
+    const { lmeOnDuty } = useContext(AtendimentoContext)
+
+    return (
+        <>
+            <Tooltip title="Voltar">
+                <IconButton
+                    component="span"
+                    disabled={!lmeOnDuty}
+                    size="large"
+                >
+                    <ArrowUpwardIcon />
+                </IconButton>
+            </Tooltip>
+        </>
+    )
+}
+
+const EditarBtn = () => {
+
 
     const { lmeOnDuty, setLmeEdit } = useContext(AtendimentoContext)
-    const { setArticleLMEDelete, setArticleLMEUpdate, setStep } = useContext(AtendimentoNavigateContext)
+    const { setArticleLMEUpdate, setStep } = useContext(AtendimentoNavigateContext)
 
 
     const handleEditar = () => {
@@ -18,42 +37,55 @@ const LmesMainAppBar = () => {
         setArticleLMEUpdate()
     }
 
+    return (
+        <>
+            <Tooltip title="Editar">
+                <IconButton
+                    component="span"
+                    onClick={handleEditar}
+                    size="large"
+                >
+                    <EditIcon />
+                </IconButton>
+            </Tooltip>
+        </>
+    )
+}
+
+const PararBtn = () => {
+
+    const { lmeOnDuty } = useContext(AtendimentoContext)
+    const { setArticleLMEDelete } = useContext(AtendimentoNavigateContext)
+
+
     const handleParar = () => {
         setArticleLMEDelete()
     }
 
     return (
         <>
-            <Tooltip title="Voltar">
-                <span>
-                    <IconButton disabled={!lmeOnDuty} size="large">
-                        <ArrowUpwardIcon />
-                    </IconButton>
-                </span>
-            </Tooltip>
-            <Tooltip title="Editar">
-                <span>
-                    <IconButton disabled={!lmeOnDuty} onClick={handleEditar} size="large">
-                        <EditIcon />
-                    </IconButton>
-                </span>
-            </Tooltip>
             <Tooltip title="Parar">
-                <span>
-                    <IconButton disabled={!lmeOnDuty} onClick={handleParar} size="large">
-                        <HighlightOffIcon />
-                    </IconButton>
-                </span>
+                <IconButton
+                    component="span"
+                    disabled={!lmeOnDuty}
+                    onClick={handleParar}
+                    size="large"
+                >
+                    <HighlightOffIcon />
+                </IconButton>
             </Tooltip>
-            <Tooltip title="Outros">
-                <span>
-                    <IconButton
-                        disabled={!lmeOnDuty}
-                        size="large">
-                        <HealingIcon />
-                    </IconButton>
-                </span>
-            </Tooltip>
+        </>
+    )
+}
+
+
+const LmesMainAppBar = () => {
+
+    return (
+        <>
+            <ReiniciarBtn />
+            <EditarBtn />
+            <PararBtn />
         </>
     )
 }
