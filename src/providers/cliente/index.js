de92840f60cc634cae2b8255b83a9e3f97c39c47
diogofdepaula <diogofdepaula@gmail.com>
2022-06-09 11:05:13 -0300

    hotfix no DoençaCID

--- src/providers/cliente/index.js
+++ src/providers/cliente/index.js
@@ -5,15 +5,12 @@ const ClienteProvider = () => {
     const [clienteContext, setClienteContext] = useState()
 
     const fetchClienteIncludes = async (param) => {
-        console.log("teste 1");
         await fetch(process.env.REACT_APP_API_URL + '/clientes/' + param)
             .then(res => {
                 if (res.ok) {
-                    console.log("teste 2");
                    return res.json()
                 }
             }).then(data => {
-                console.log("teste 3");
                 setClienteContext(data)
             })
     }
--- src/utils/inquiries.js
+++ src/utils/inquiries.js
@@ -12,6 +12,6 @@ export const DoençaCID = (param) => {
     if (['M050', 'M051', 'M052', 'M053', 'M058', 'M060', 'M068'].includes(param)) return "ar"
     if (['M080', 'M051', 'M081', 'M082', 'M083', 'M084', 'M088', 'M089'].includes(param)) return "aij"
     if (['M070', 'M072', 'M073'].includes(param)) return "ap"
-    if (['M45', 'M0468'].includes(param)) return "ea"
+    if (['M45', 'M468'].includes(param)) return "ea"
     return 'outro'
 }
