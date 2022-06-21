d52dd013ac2f5f7876b2af31a7409442aa216784
diogofdepaula <diogofdepaula@gmail.com>
2022-06-21 10:59:36 -0300

    erro de digitação Prednisona

--- src/components/print/relatorio/component/linha6/index.js
+++ src/components/print/relatorio/component/linha6/index.js
@@ -116,7 +116,7 @@ const Linha6LME = () => {
             'Glicocorticoide',
             '------',
             [
-                'Prednsiona'
+                'Prednisiona'
             ],
         ],
         [
