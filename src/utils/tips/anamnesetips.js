ef7107741c696474bd24173de1a9c6f80e39ee2e
diogofdepaula <diogofdepaula@gmail.com>
2022-06-28 10:48:46 -0300

    Tip para AR2020

--- src/utils/tips/anamnesetips.js
+++ src/utils/tips/anamnesetips.js
@@ -3,6 +3,7 @@ import { Box, Chip, Divider, IconButton, Paper } from '@mui/material';
 import React, { useContext } from 'react';
 import { AtendimentoContext } from '../../pages/atendimento';
 import { CriteriosLme } from '../criteriosdoencas';
+import { DoençaCID } from '../inquiries';
 import ICAD from './das28';
 
 const AnamneseTips = () => {
@@ -37,18 +38,13 @@ const AnamneseTips = () => {
                     :
                     param[2] !== undefined
                         ?
-                        {
-                            ...lmeEdit.relatorio, [param[1]]: true
-                            //(fazer um if para ar2010)
-                        }
+                        { ...lmeEdit.relatorio, [param[1]]: true }
                         :
                         { ...lmeEdit.relatorio }
         })
     }
 
     const handleClickChipVirgula = param => () => {
-
-        console.log(param);
         setLmeEdit({
             ...lmeEdit,
             anamnese: lmeEdit.anamnese.concat(param[0]).concat(', '),
@@ -59,13 +55,100 @@ const AnamneseTips = () => {
                     :
                     param[2] === undefined
                         ?
-                        {
-                            ...lmeEdit.relatorio, [param[1]]: true
-                            //(fazer um if para ar2010)
-                        }
+                        { ...lmeEdit.relatorio, [param[1]]: true }
                         :
                         { ...lmeEdit.relatorio }
         })
+    }
+
+    const CriteriosAR2020 = () => {
+
+        const handleClickChip2020 = (paramA, paramB) => () => {
+            setLmeEdit({
+                ...lmeEdit,
+                anamnese: lmeEdit.anamnese.concat('\n').concat(paramB[0]),
+                relatorio: lmeEdit.relatorio === null
+                    ?
+                    null
+                    :
+                    { ...lmeEdit.relatorio, [paramA]: paramB[1] }
+            })
+        }
+
+        const handleClickChipVirgula2020 = (paramA, paramB) => () => {
+            setLmeEdit({
+                ...lmeEdit,
+                anamnese: lmeEdit.anamnese.concat(paramB[0]).concat(', '),
+                relatorio: lmeEdit.relatorio === null
+                    ?
+                    null
+                    :
+                    { ...lmeEdit.relatorio, [paramA]: paramB[1] }
+            })
+        }
+
+        return (
+            <>
+                {CriteriosLme(lmeEdit)[1].map(x =>
+                    x[1].map(y =>
+                        <Box key={y[0]}>
+                            <Chip
+                                label={y[0]}
+                                variant="outlined"
+                                onClick={handleClickChipVirgula2020(x[0][1], y)}
+                            />
+                            <IconButton
+                                draggable
+                                onClick={handleClickChip2020(x[0][1], y)}
+                                size="small">
+                                <FormatTextdirectionLToRIcon />
+                            </IconButton>
+                        </Box>
+                    ))
+                }
+                {CriteriosLme(lmeEdit)[0].map((p, i) =>
+                    <Box key={i}>
+                        <Chip
+                            label={p[0]}
+                            variant="outlined"
+                            onClick={handleClickChipVirgula(p)}
+                        />
+                        <IconButton
+                            draggable
+                            onClick={handleClickChip(p)}
+                            size="small">
+                            <FormatTextdirectionLToRIcon />
+                        </IconButton>
+                    </Box>
+                )}
+            </>
+        )
+
+    }
+
+    const Criterios = () => {
+
+        if (DoençaCID(lmeEdit.cid10) === 'ar') return <CriteriosAR2020 />
+
+        return (
+            <>
+                {CriteriosLme(lmeEdit)[0].map((p, i) =>
+                    <Box key={i}>
+                        <Chip
+                            label={p[0]}
+                            variant="outlined"
+                            onClick={handleClickChipVirgula(p)}
+                        />
+                        <IconButton
+                            draggable
+                            onClick={handleClickChip(p)}
+                            size="small">
+                            <FormatTextdirectionLToRIcon />
+                        </IconButton>
+                    </Box>
+                )}
+            </>
+        )
     }
 
     return (
@@ -100,23 +183,10 @@ const AnamneseTips = () => {
                         p: 1,
                         display: 'flex',
                         flexDirection: 'column',
+                        gap: 0.5
                     }}
                 >
-                    {CriteriosLme(lmeEdit)[0].map((p, i) =>
-                        <Box key={i}>
-                            <Chip
-                                label={p[0]}
-                                variant="outlined"
-                                onClick={handleClickChipVirgula(p)}
-                            />
-                            <IconButton
-                                draggable
-                                onClick={handleClickChip(p)}
-                                size="large">
-                                <FormatTextdirectionLToRIcon />
-                            </IconButton>
-                        </Box>
-                    )}
+                    <Criterios />
                 </Box>
             </Paper>
             <ICAD />
