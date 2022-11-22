91cfa58cb166b5f9928e060c67eefda3bb5b7423
diogofdepaula <diogofdepaula@gmail.com>
2022-11-22 14:39:11 -0300

    apagando comentários

--- src/components/appbar/primaryappbar.js
+++ src/components/appbar/primaryappbar.js
@@ -19,63 +19,51 @@ const PrimaryAppBar = () => {
 
     return (
         <>
-            {/* <Box
+            <AppBar
                 sx={{
-                    display: 'flex',
-                    backgroundColor: 'red'
+                    position: 'fixed',
+                    zIndex: 1251,
                 }}
-            > */}
-                <AppBar
-                    sx={{
-                        position: 'fixed',
-                        zIndex: 1251,
-                    }}
-                >
-                    <Toolbar>
-                        <Box
-                            sx={{
-                                // menos 16 do padding
-                                display: 'flex',
-                                alignItems: 'center',
-                                //width: '216px'
-                                // se somente o icone da casa
-                                width: '44px',
-                            }}
-                        >
-                            <HealthAndSafetyIcon
-                                fontSize='large'
-                                onClick={handleHome}
-                            />
-                        </Box>
-                        <Box
-                            style={{
-                                flexGrow: 1,
-                                marginLeft: 20,
-                            }}
-                        >
-                            <ClienteSet />
-                        </Box>
-                        <Box>
-                            <IconButton color="inherit" size="large" >
-                                <Badge badgeContent={77} color="secondary">
-                                    <MailIcon />
-                                </Badge>
-                            </IconButton>
-                            <IconButton color="inherit" size="large">
-                                <Badge badgeContent={1} color="secondary">
-                                    <NotificationsIcon />
-                                </Badge>
-                            </IconButton>
-                            <IconButton edge="end" color="inherit" size="large">
-                                <AccountCircle />
-                            </IconButton>
-                            <IconButton color="inherit" size="large">
-                                <MoreIcon />
-                            </IconButton>
-                        </Box>
-                    </Toolbar>
-                </AppBar>
-            {/* </Box> */}
+            >
+                <Toolbar>
+                    <Box
+                        sx={{
+                            width: '24px',
+                        }}
+                    >
+                        <HealthAndSafetyIcon
+                            fontSize='large'
+                            onClick={handleHome}
+                        />
+                    </Box>
+                    <Box
+                        style={{
+                            flexGrow: 1,
+                            marginLeft: 20,
+                        }}
+                    >
+                        <ClienteSet />
+                    </Box>
+                    <Box>
+                        <IconButton color="inherit" size="large" >
+                            <Badge badgeContent={77} color="secondary">
+                                <MailIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton color="inherit" size="large">
+                            <Badge badgeContent={1} color="secondary">
+                                <NotificationsIcon />
+                            </Badge>
+                        </IconButton>
+                        <IconButton edge="end" color="inherit" size="large">
+                            <AccountCircle />
+                        </IconButton>
+                        <IconButton color="inherit" size="large">
+                            <MoreIcon />
+                        </IconButton>
+                    </Box>
+                </Toolbar>
+            </AppBar>
         </>
     )
 }
