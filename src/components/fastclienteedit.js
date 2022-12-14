2e8cb3a8647cdde20329967ac2af6af17099a45b
diogofdepaula <diogofdepaula@gmail.com>
2022-12-14 16:55:28 -0300

    resetcliente no fast

--- src/components/fastclienteedit.js
+++ src/components/fastclienteedit.js
@@ -8,7 +8,7 @@ import FormCliente from './formcliente';
 
 const FastClienteEdit = ({ handleClose, open }) => {
 
-    const { clienteContext, setClienteContext } = useContext(ClienteContext)
+    const { clienteContext, setClienteContext, setResetCliente } = useContext(ClienteContext)
     const { setPageAtendimento } = useContext(NavigateContext)
 
     const handleChange = event => {
@@ -22,22 +22,7 @@ const FastClienteEdit = ({ handleClose, open }) => {
             body: JSON.stringify(clienteContext)
         }).then(data => {
             if (data.ok) {
-                fetch(process.env.REACT_APP_API_URL + '/clientes/' + clienteContext.id)
-                    .then(res => {
-                        if (res.ok) {
-                            return res.json()
-                        }
-                    }).then(data => {
-                        // isso inclui os includes Prescricao nas LMES sem precisar
-                        // fazer uma pesquisa duplicada no bando de dados
-                        const lmes = data.lmes.map(l => {
-                            let n = data.prescricoes.filter(p => p.lmeId === l.id)
-                            return { ...l, prescricoes: n }
-                        })
-                        return { ...data, lmes: lmes }
-                    }).then(cliente => {
-                        setClienteContext(cliente)
-                    })
+                setResetCliente()
             }
         }).finally(() => {
             setPageAtendimento()
