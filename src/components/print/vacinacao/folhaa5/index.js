078f8d268f8fec0abb24d284da54e23ecb7a5cfa
diogofdepaula <diogofdepaula@gmail.com>
2023-05-04 10:15:59 -0300

    Recomenda-se no lugar de indica-se

--- src/components/print/vacinacao/folhaa5/index.js
+++ src/components/print/vacinacao/folhaa5/index.js
@@ -57,7 +57,7 @@ const VacinacaoA5 = ({ vacinacao, tipo }) => {
                             typography: 'h6',
                         }}
                     >
-                        Indica-se vacinação, caso não tenham sido feitas, para:
+                        Recomenda-se vacinação, caso não tenham sido feitas, para:
                     </Box>
                     <Box
                         sx={{
