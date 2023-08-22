b92ffbab18080494f8aaef7569b8edec9f8581c4
diogofdepaula <diogofdepaula@gmail.com>
2023-08-22 18:01:39 -0300

    hotfix

--- src/pages/avulso/requisicao/index.js
+++ src/pages/avulso/requisicao/index.js
@@ -61,10 +61,13 @@ const Requisicoes = ({ handleAdicionarRequisicao, itemEdit, ind }) => {
     }
 
     const handleProcedimentoRemove = (param) => {
+
+        console.log(        requisicao.selecionados
+            );
         setRequisicao({
             ...requisicao,
             indice: ind.current,
-            selecionados: requisicao.selecionados.filter(w => w.original.toString().toLowerCase() !== param.original.toString().toLowerCase())
+            selecionados: requisicao.selecionados.filter(w => w.mod.toString().toLowerCase() !== param.mod.toString().toLowerCase())
         })
     }
 
