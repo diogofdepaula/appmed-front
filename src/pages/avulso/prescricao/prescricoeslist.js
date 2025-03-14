28f42f007865c0700c248965678642324eb49b2a
diogofdepaula <diogofdepaula@gmail.com>
2025-03-14 15:28:39 -0300

    Colflex HIALU

--- src/pages/avulso/prescricao/prescricoeslist.js
+++ src/pages/avulso/prescricao/prescricoeslist.js
@@ -383,7 +383,7 @@ const UciiHaMsm = {
         farmaco: "Colágeno tipo II não desnaturado + Ácido hialurônico + Metilsulfonilmetano",
         nomescomerciais: [
             {
-                nomefantasia: "Colaten FORCE ou Motilex HA",
+                nomefantasia: "Colaten FORCE, Motilex HA ou Colflex HIALU",
             },
         ]
     },
